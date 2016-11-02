aff01556d1a26bd4fb9c762979072fc27f47b76d
VaDikOo8 <vadikoo8.patron@gmail.com>
2016-11-02 23:15:57 +0300

    Запуск тестов в разных браузерах
    Добавлено условие для запуска тестов в различных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,6 +2,10 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -9,7 +13,13 @@ import java.util.concurrent.TimeUnit;
  * Created by Вадим on 29.10.2016.
  */
 public class ApplicationManager {
-  WebDriver wd = new ChromeDriver();
+
+  WebDriver wd;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   private ContactHelper contactHelper;
   private SessionHelper sessionHelper;
@@ -17,6 +27,15 @@ public class ApplicationManager {
   private GroupHelper groupHelper;
 
   public void init() {
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    } else if (browser.equals(BrowserType.OPERA)) {
+      wd = new OperaDriver();
+    }
     wd.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
     wd.get("http://localhost/addressbook/");
     contactHelper = new ContactHelper(wd);
