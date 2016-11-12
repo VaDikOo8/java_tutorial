50308800fc1f13a8975150845595a6970255cc47
VaDikOo8 <vadikoo8.patron@gmail.com>
2016-11-12 18:07:53 +0300

    Оптимизация теста модификации контакта
    - Добавлена проверка в тестах через вспомогательный метод haveFullList
    на наличие записей групп на странице групп. В случае отсутствия записей
    производится создание группы, а затем ее модификация.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -11,11 +12,16 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
+    if (app.getGroupHelper().haveFullList() == false) {
+      app.getGroupHelper().initGroupCreation();
+      app.getGroupHelper().fillGroupForm(new GroupData("Group2", "group_header2", "group_footer2"));
+      app.getGroupHelper().submitGroupCreation();
+      app.getNavigationHelper().returnToGroupPage();
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("Group2", "group_header2", null));
+    app.getGroupHelper().fillGroupForm(new GroupData("Group3", "group_header3", null));
     app.getGroupHelper().submitGroupModification();
     app.getNavigationHelper().returnToGroupPage();
   }
-
 }
